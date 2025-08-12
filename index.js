f5df211114d56f2fd3ea2f18a6c47c5cc813ee62
Breeana Cordova <bcordo12@wgu.edu>
2025-08-11 18:36:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // index.js
-console.log("Vaultora is running!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Vaultora server listening on ${PORT}`);
+  });
+}
+
+module.exports = app;
