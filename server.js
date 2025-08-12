d39c3a9b3e4052e5f8b6be099e299a7ff1baa70c
Breeana Cordova <bcordo12@wgu.edu>
2025-08-11 21:07:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// server.js
+require('dotenv').config();
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Vaultora server listening on ${PORT}`);
 });
